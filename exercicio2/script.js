c848c12ec16a60749067cc0afe41e500332627ac
layanemnzswok@gmail.com <layanemnzswork@gmail.com>
2022-09-06 16:13:48 -0300

    Exercios Condicionais a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,23 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = Number(prompt("Qual é a sua idade?"))
+let isMaiorIdade = confirm("Já terminou o Ensino Médio?")
+let isCursandoOutraFaculdade = 0;
 
-if(){
-
+if(idade >=8) {
+console.log("A pessoa é maior de idade")
 }else{
-	
+	console.log("A pessoa é menor de idade")
+}
+
+if (isMaiorIdade === true) {
+    console.log("A pessoa terminou o Ensino Médio")
+let isCursandoOutraFaculdade = confirm("Você está cursando alguma faculdade?")
+resposta = isCursandoOutraFaculdade;
+} else {
+    console.log("A pessoa não terminou o Ensino Médio")
+}
+
+if (resposta !== true) {
+    console.log("A pessoa NÃO está cursando uma faculdade")
+} else {
+    console.log("A pessoa está cursando uma faculdade")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,36 @@
-let nacionalidade //= adicione o prompt aqui
+/*let nacionalidade = prompt("Escreva aqui sua nacionalidade").toLocaleLowerCase()
+const nacionalidade1 = "brasileira"
+const nacionalidade2 = "argentina"
+const nacionalidade3 = "uruguaia"
+const nacionalidade4 = "chilena"
+const nacionalidade5 = "colombiana"
+
+if (nacionalidade === nacionalidade1) {
+    console.log("A sua nacionalidade é:" + "nacionalidade1")
+} else if (nacionalidade === nacionalidade2) {
+    console.log("A sua nacionalidade é:" + "nacionalidade2") 
+} else if (nacionalidade === nacionalidade3) {
+    console.log("A sua nacionalidade é:" + "nacionalidade3")
+} else if (nacionalidade === nacionalidade4) {
+    console.log("A sua nacionalidade é:" + "nacionalidade4")
+} else if (nacionalidade === nacionalidade5) {
+    console.log("A sua nacionalidade é:" + "nacionalidade5")
+} else { 
+     console.log('Nacionalidade não encontrada')*/
+
+
+     let nacionalidade = prompt("Qual é a sua nacionalidade?").toLocaleLowerCase()
+     const nasci = nacionalidade
+
+     if (nasci === "brasileira") {
+        console.log("Sua nacionalidade é brasileira.")
+     } else if (nasci === "chilena") {
+        console.log("Sua nacionalidade é chilena.")
+     } else if (nasci === "colombiana") {
+        console.log("Sua nacionalidade é colombiana.")
+     } else if (nasci === "argentina.") {
+        console.log("Sua nacionalidade éargentina.")
+     } else if (nasci === "uruguai") {
+        console.log("Sua nacionalidade é uruguaia.")
+     } else
+     console.log("Nacionalidade não encontrada.")
